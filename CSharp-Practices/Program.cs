1f7f1a22bb55cf7213e592d6e081888eeb8fb06f
homoro <mr.homoro@gmail.com>
2020-06-24 13:55:40 +0800

    Add "System.ValueTuple" Using nested Tuples

--- CSharp-Practices/Program.cs
+++ CSharp-Practices/Program.cs
@@ -10,20 +10,30 @@ namespace CSharp_Practices
             // Exam2();
             // Exam3();
             // Exam4();
-            Exam5();
-            Console.ReadKey();
+            // Exam5();
+            // Console.ReadKey();
+
+            ///
+            var person = TupleSample2.GetNewTuple();
+            Console.WriteLine($"Name : {person.Item1} Email : {person.Item2} Age : {person.Item3}");
+
+            var Homoro = TupleSample2.GetPerson();
+            Console.WriteLine($"{Homoro.name} - {Homoro.email} - {Homoro.age}");
+
+            Console.ReadLine();
+
         }
 
         static void Exam1()
         {
-            var person = new Tuple<string, string, int>("Mr.", "Homoro", 26);
-            Console.WriteLine($"Name : {person.Item1} Family : {person.Item2} Age : {person.Item3}");
+            var person = new Tuple<string, string, int>("Homoro", "mr.Homoro@gmail.com", 26);
+            Console.WriteLine($"Name : {person.Item1}, Email : {person.Item2}, Age : {person.Item3}.");
         }
 
         static void Exam2()
         {
-            var site = Tuple.Create("mrhomoro.com", "mr.homoro@gmail.com", "Mr.Homoro", "0182378258", "1");
-            Console.WriteLine($"{site.Item1} - {site.Item2} - {site.Item3} - {site.Item4} - {site.Item5}");
+            var info = Tuple.Create("mrhomoro.com", "mr.homoro@gmail.com", "Mr.Homoro", "0182378258", "1");
+            Console.WriteLine($"{info.Item1} - {info.Item2} - {info.Item3} - {info.Item4} - {info.Item5}");
         }
 
         static void Exam3()
@@ -35,28 +45,54 @@ namespace CSharp_Practices
 
         static void Exam4()
         {
-            TupleSample ts = new TupleSample();
+            TupleSample1 ts = new TupleSample1();
             var person = Tuple.Create("Mr.", "Homoro", 26);
             ts.SetTupleInMethod(person);
         }
 
         static void Exam5()
         {
-            var person = TupleSample.GetTuple();
-            Console.WriteLine($"Name : {person.Item1} Family : {person.Item2} Age: {person.Item3}");
+            var person = TupleSample1.GetTuple();
+            Console.WriteLine($"Name : {person.Item1}, Email : {person.Item2}, Age: {person.Item3}.");
         }
     }
 
-    public class TupleSample
+    public class TupleSample1
     {
         public void SetTupleInMethod(Tuple<string, string, int> person)
         {
-            Console.WriteLine($"Name : {person.Item1} Family : {person.Item2} Age: {person.Item3}");
+            Console.WriteLine($"Name : {person.Item1}, Email : {person.Item2}, Age: {person.Item3}.");
         }
 
         public static Tuple<string, string, int> GetTuple()
         {
             return Tuple.Create("Mr.", "Homoro", 26);
+        }
+    }
+
+
+    /// Note:
+    /// using System.ValueTuple
+    /// 
+
+    class TupleSample2
+    {
+        public static (string, string, int) GetNewTuple()
+        {
+            string name = "Homoro";
+            string email = "mr.Homoro@gmail.com";
+            int age = 26;
+
+            return (name, email, age);
+        }
+
+        public static (string name, string email, int age) GetPerson()
+        {
+            string name = "Homoro";
+            string email = "mr.Homoro@gmail.com";
+            int age = 26;
+
+            return (name, email, age);
         }
 
     }
