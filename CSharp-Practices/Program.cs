4a854c86555eac071c9dbbd454753aabc8528bbb
homoro <mr.homoro@gmail.com>
2020-06-24 14:16:07 +0800

    Deconstruction in C# 7.0. is a process of splitting a variable value into parts and storing them into new variables. This could be useful when a variable stores multiple values such as a tuple.

--- CSharp-Practices/Program.cs
+++ CSharp-Practices/Program.cs
@@ -3,11 +3,35 @@
 namespace CSharp_Practices
 {
     class Program
-    { 
+    {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello CSharp version 7 !"); 
+            Person p = new Person("Homoro", "mr.homoro@gmail.com");
+            //Console.WriteLine($"Name: {p.Name}, Email: {p.Email}");
 
+            // using deconstruct:
+            var (Name, email) = p;
+            Console.WriteLine($"Name: {Name}, Email: {email}");
         }
+    }
+
+    class Person
+    {
+        public string Name { get; }
+        public string Email { get; }
+
+        public Person(string name, string email)
+        {
+            Name = name;
+            Email = email;
+        }
+
+        public void Deconstruct(out string name, out string email)
+        {
+            name = Name;
+            email = Email;
+        }
+
+
     }
 }
